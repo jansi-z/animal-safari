3dcbc8aa47c88f386f0a8a7defeaabc38900513a
megancallenbach <megancallenbach@gmail.com>
2017-10-03 15:57:00 +0200

    Input bar and guess action

--- src/containers/InputBar.js
+++ src/containers/InputBar.js
@@ -1,11 +1,40 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
 import TextField from 'material-ui/TextField';
+import addGuess from '../actions/games/guess'
+import { connect } from 'react-redux'
 
-const InputBar = () => (
-  <TextField
-        hintText="Guess an Animal"
-        floatingLabelText="Guess:"
-      />
-)
+class InputBar extends PureComponent {
+  submitGuess(event) {
+    event.preventDefault();
+    // debugger;
+    const guess = this.refs.guess.getValue();
+    const gameId = this.props.currentGame;
+    const player = this.props.currentUser._id;
+    const guessData = { player: player, guess: guess }
 
-export default InputBar
+    this.props.addGuess(gameId, guessData);
+    console.log(guessData)
+    console.log(gameId)
+
+  }
+
+  render() {
+
+    return (
+      <form onSubmit={this.submitGuess.bind(this)}>
+        <TextField
+          hintText="Guess an Animal"
+          floatingLabelText="Guess:"
+          ref="guess"
+        />
+      </form>
+    )
+  }
+
+}
+
+const mapStateToProps = ({ currentGame, currentUser }) => ({ currentGame, currentUser })
+
+const mapDispatchToProps = { addGuess }
+
+export default connect (mapStateToProps, mapDispatchToProps)(InputBar)
