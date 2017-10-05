8c185df6feb3a8fad3e8ff1129999cda7ae3a22c
megancallenbach <megancallenbach@gmail.com>
2017-10-05 07:38:22 +0200

    Image container shows animal

--- src/containers/Image.js
+++ src/containers/Image.js
@@ -1,18 +1,28 @@
 import React, { PureComponent } from 'react'
 import { connect } from 'react-redux'
-import panda from '../images/5.jpg'
 
 class Image extends PureComponent {
 
   render() {
+    const imagePath = "http://res.cloudinary.com/meganc94/image/upload/v1507117505/"
+    const time = this.props.game.time / 5 - 4;
+    const { animal } = this.props.game
     return (
       <div className="Image">
-        <img src={panda} alt="hedgehog"/>
+        <img src={`${imagePath}${animal}-${time}.jpg`} alt="hedgehog"/>
       </div>
     )
   }
 }
 
-const mapStateToProps = ({ games, currentGame }) => ({ games, currentGame })
+const mapStateToProps = ({ games, currentGame }) => {
+  const game = games.filter((g) => (g._id === currentGame))[0]
+
+  return {
+    game,
+    games,
+    currentGame,
+  }
+}
 
 export default connect(mapStateToProps)(Image)
